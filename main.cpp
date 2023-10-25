946d8cb6cd4ca809e8592ec7917cfa783a056aff
Diefudu2 <andrescastromoreno158@gmail.com>
2023-10-25 10:56:18 -0600

    Cambio de botones a una matriz dibujada

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <QApplication>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -6,6 +7,12 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <QtWidgets>
+#include <QGridLayout>
+#include <QLabel>
+#include <QPixmap>
+
+QString Path = "/home/andresc/Escritorio/Client-Samurai/";
+
 
 // Estructura de un punto en el tablero
 struct Point {
@@ -13,7 +20,7 @@ struct Point {
     int y;
 };
 
-// Función para enviar datos al servidor
+
 bool sendToServer(const std::string& serverAddress, int serverPort, const std::string& data) {
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -33,19 +40,29 @@ bool sendToServer(const std::string& serverAddress, int serverPort, const std::s
 }
 
 int main(int argc, char *argv[]) {
+
     std::string serverAddress = "127.0.0.1";  // Dirección IP del servidor
     int serverPort = 12345;  // Puerto del servidor
-
     QApplication app(argc, argv);
 
     // Crear la ventana principal
     QWidget window;
     window.setWindowTitle("Samurai Stronghold GBP");
 
+    // Crear una matriz para representar el tablero
+    const int boardSize = 10;
+    std::vector<std::vector<int>> gameBoard(boardSize, std::vector<int>(boardSize, 0));
+
     // Crear un layout de cuadrícula (10x10)
     QGridLayout *gridLayout = new QGridLayout(&window);
 
-    // Función para enviar las coordenadas al servidor
+    // Crear un QLabel con la imagen del samurái redimensionada a 50x50
+    QPixmap samuraiPixmap(Path + "rojo1.png");
+    samuraiPixmap = samuraiPixmap.scaled(50, 50, Qt::KeepAspectRatio);
+    QLabel *samuraiLabel = new QLabel(&window);
+    samuraiLabel->setPixmap(samuraiPixmap);
+
+
     auto sendCoordinatesToServer = [&serverAddress, serverPort](int row, int col) {
         std::string coordinates = std::to_string(row) + "," + std::to_string(col);
         if (sendToServer(serverAddress, serverPort, coordinates)) {
@@ -55,26 +72,29 @@ int main(int argc, char *argv[]) {
         }
     };
 
-    // Crear el tablero y agregar botones
-    for (int row = 0; row < 10; ++row) {
-        for (int col = 0; col < 10; ++col) {
-            QPushButton *button = new QPushButton;
-            button->setFixedSize(70, 70);
-            gridLayout->addWidget(button, row, col);
 
-            // Conectar el evento clic del botón a la función de envío de coordenadas
-            QObject::connect(button, &QPushButton::clicked, [row, col, &sendCoordinatesToServer]() {
-                sendCoordinatesToServer(row, col);
-            });
+    // Función para actualizar la posición del samurái en la ventana
+    auto updateSamuraiPosition = [&samuraiLabel](int newRow, int newCol) {
+        // Actualiza la posición del samurái en la ventana
+        samuraiLabel->setGeometry(newCol * 50, newRow * 50, 50, 50);
+    };
+
+    // Agregar QLabel del samurái al layout en la posición inicial (0, 0)
+    gridLayout->addWidget(samuraiLabel, 0, 0);
+    gameBoard[0][0] = 1;
+
+    // Agregar etiquetas como fondo para el tablero (10x10)
+    for (int row = 0; row < boardSize; ++row) {
+        for (int col = 0; col < boardSize; ++col) {
+            QLabel *cellLabel = new QLabel;
+            cellLabel->setFrameStyle(QFrame::Panel | QFrame::Sunken);
+            cellLabel->setFixedSize(50, 50);
+            gridLayout->addWidget(cellLabel, row, col);
+
         }
     }
 
-    // Crear un QLabel con la imagen del samurái
-    QLabel *samuraiLabel = new QLabel(&window);
-    QPixmap samuraiImage("/home/fernanda/Downloads/rojo.png");
-    samuraiLabel->setPixmap(samuraiImage);
-    samuraiLabel->setGeometry(10, 10, 70, 70);  // Ajusta la ubicación y tamaño del QLabel
-
+    // Mostrar la ventana
     window.show();
 
     return app.exec();
